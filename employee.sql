d386aea7be33216b71b6985da1afc72977d74f56
KatukhaAlexey <146508056+KatukhaAlexey@users.noreply.github.com>
2023-12-25 10:45:34 +1100

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -14,3 +14,13 @@ boss_ID SERIAL PRIMARY KEY,
 employee_ID INTEGER NOT NULL REFERENCES employee(employee_ID),
 departmint_ID INTEGER NOT NULL REFERENCES departmint(departmint_ID)
 );
+
+/* Если один сотрудник не может быть начальником в 2 и более отделов, что нужно указать, что employee_ID должно быть UNIQUE
+
+CREATE TABLE IF NOT EXISTS boss (
+boss_ID SERIAL PRIMARY KEY,
+employee_ID UNIQUE INTEGER NOT NULL REFERENCES employee(employee_ID),
+departmint_ID INTEGER NOT NULL REFERENCES departmint(departmint_ID)
+);
+
+*/
